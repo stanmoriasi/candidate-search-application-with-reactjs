dfcd1da72303d0e7948b270c6392aa16846ce22c
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-15 15:37:38 -0700

    add candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,14 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    location: string;
+    skills: string[];
+    experience: number;
+    education: string;
+    resume: string;
+}
+
+export default Candidate;
