0128df46c320d00b13ff9ac8ca32cf5641478497
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-15 15:38:04 -0700

    update gitignore

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,7 +1,19 @@
-import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
+//import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { searchGithub } from '../api/API';
 
 const CandidateSearch = () => {
+  useEffect(() => {
+    async function fetchData() {
+      // const user = await searchGithubUser('stanmoriasi');
+      const users = await searchGithub();
+      console.log(users);
+          }
+    fetchData();
+  }, []);
+
+
+
   return <h1>CandidateSearch</h1>;
 };
 
