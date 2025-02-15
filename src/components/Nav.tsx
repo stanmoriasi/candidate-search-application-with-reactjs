fa0eb443f3b8e176b63ebb2213d7d4d775923bce
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-15 15:36:44 -0700

    Update nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,16 @@
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <>
+    <nav >
+      <ul className="nav">
+        <li className="nav-item"><a href="/" className="nav-link">Home</a></li>
+        <li className="nav-item"><a href="/candidate" className="nav-link">Potential Candidates</a></li>
+      </ul>
+
+    </nav>
+    </>
   )
 };
 
