aa8fc021740f10ce08c55f52a4f525db022e35ce
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-18 21:02:19 -0700

    update vite environment directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
+  envDir: './environment',
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3010,
   },
 });
