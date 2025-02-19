947ddc536ffc8c5e14a77e64826c60c2af1e0c06
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-18 21:02:59 -0700

    Debug token issue

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -1,24 +1,25 @@
 const searchGithub = async () => {
   try {
     const start = Math.floor(Math.random() * 100000000) + 1;
-    // console.log(import.meta.env);
     const response = await fetch(
       `https://api.github.com/users?since=${start}`,
       {
         headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+          //Authorization: `Bearer ${VITE_GITHUB_TOKEN}`,
+          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`
         },
       }
+      
     );
-    // console.log('Response:', response);
+    console.log('Response:', response);
     const data = await response.json();
     if (!response.ok) {
       throw new Error('invalid API response, check the network tab');
     }
-    // console.log('Data:', data);
+     //console.log('Data:', data);
     return data;
   } catch (err) {
-    // console.log('an error occurred', err);
+   console.log('an error occurred', err);
     return [];
   }
 };
@@ -34,6 +35,7 @@ const searchGithubUser = async (username: string) => {
     if (!response.ok) {
       throw new Error('invalid API response, check the network tab');
     }
+    
     return data;
   } catch (err) {
     // console.log('an error occurred', err);
