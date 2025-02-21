a5c0075a736979a2ae8b876242f8a5d1fcc12f6f
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-20 21:03:30 -0700

    Update vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
-  server: {
-    port: 3001,
-  },
+  // server: {
+  //   port: 3001,
+  // },
 });
