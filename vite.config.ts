58e1574d4f15e21c0b5bf10478f094ea1b968a62
Stan Moriasi <stanmoriasi@yahoo.com>
2025-04-21 12:32:25 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './environment',
-  plugins: [react()],
-  // server: {
-  //   port: 3001,
-  // },
   server: {
     port: 3000,
-    host: "0.0.0.0",
-    allowedHosts: [
-      "candidate-search-application-with-reactjs-0v9s.onrender.com",
-    ],
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/auth': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false
+      },
+    },
   },
 });
