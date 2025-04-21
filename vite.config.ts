93a8d5eda2604bf07e7a49222862bea8aa34cba9
Stan Moriasi <stanmoriasi@yahoo.com>
2025-04-21 12:51:34 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,10 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
       '/auth': {
-        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       },
